1f03f224ec8f5530c4a17dcff9c4d55e01a6ebed
System Administrator <root@makedeMacBook-Pro.local>
2018-02-26 16:26:06 +0800

    start

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
+
+// npm
+//
+// validate-pm-package-name - 校验npm名称是否被使用
+// cross-spawn - 跨平台执行spawn
+// server - 版本校验
+// tmp - 临时文件和文件夹
+// tar-pack - 压缩解压
+// hyperquest - http stream
+// commander - 命令行工具
+// fs-extra - 文件操作
+//
+// process.chdir - 改变工作目录
+
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
 var path = require('path')
