abdea6e1e04e16ed5ad391be68f80750f394a9c5
System Administrator <root@makedeMacBook-Pro.local>
2018-02-27 11:59:48 +0800

    build

--- scripts/build.js
+++ scripts/build.js
@@ -7,4 +7,18 @@ var fs = require('fs')
 
 var webpackConfig = require('../config/webpack-prod.js')
 
-var compiler = webpack(webpackConfig,(err,status)=>{})
+var compiler = webpack(webpackConfig)
+
+compiler.apply(new webpack.ProgressPlugin());
+
+compiler.run((err,stats)=>{
+
+  if(err){
+    return console.error(err)
+  }
+  console.log(stats.toString({
+    chunks:false,
+    colors:true
+  }))
+
+})
