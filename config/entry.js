2f47d8c5314258e02521335968c6ec175542c22a
System Administrator <root@makedeMacBook-Pro.local>
2018-03-22 10:44:14 +0800

    entry

--- config/entry.js
+++ config/entry.js
@@ -4,29 +4,29 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var cwd = process.cwd()
 var pageDir=path.join(cwd,'./src/page')
 var folders=fs.readdirSync(pageDir)
-var entry={}
-var htmlConfig=[]
-
+var configs = []
 
 // 需要忽略的页面，比如一些已经下线的页面,还有.DS_Store
 var ignore = ['.DS_Store']
 
 for(var i=0;i<folders.length;i++){
+  let config = {
+    entry:{},
+    plugins:[]
+  }
   if(ignore.indexOf(folders[i])>-1){
     continue;
   }
-  entry[folders[i]]=[require.resolve(path.join(pageDir,folders[i]))]
-  htmlConfig.push(
+  config.entry[folders[i]]=[require.resolve(path.join(pageDir,folders[i]))]
+  config.plugins.push(
     new HtmlWebpackPlugin({
       chunks: ['manifest','zzlib',folders[i]],
       template:'./webpack/common/template.html',
       filename:`${folders[i]}.html`
     })
   )
+  configs.push(config)
 }
 
 
-module.exports={
-  entry,
-  htmlConfig
-}
+module.exports = configs
