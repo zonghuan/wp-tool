b3831ff8a681cabda67f4bad046a012058ec9f0e
huanhuan <huanhuan@xianzaishi.com>
2018-04-01 19:05:10 +0800

    Revert "entry"
    This reverts commit 2f47d8c5314258e02521335968c6ec175542c22a.

--- config/webpack-dev-multi.js
+++ config/webpack-dev-multi.js
@@ -5,21 +5,36 @@ var path = require('path')
 var webpack = require('webpack')
 var fs = require('fs')
 var util = require('./util.js')
-var lib = require('./util.js').getLib()
 
-var configs = require('./entry.js')
+var {entry,htmlConfig} = require('./entry.js')
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const cwd = process.cwd()
 
-for(var i=0;i<configs.length;i++){
-  let config = configs[i]
-  for(let j in config.entry){
-    config.entry[j].unshift(require.resolve('webpack-dev-server/client') + '?/')
-    config.entry[j].unshift(require.resolve('webpack/hot/dev-server'))
-  }
+// 为每一个entry添加hot-replace
+var devEntry = Object.assign({},entry)
+for(var i in devEntry){
+  devEntry[i] = [
+    // hot-replace
+    require.resolve('webpack-dev-server/client') + '?/',
+    require.resolve('webpack/hot/dev-server')
+  ].concat(devEntry[i])
+}
 
-  config.plugins = [
+var config = {
+  devtool: 'inline-source-map',
+  entry:devEntry,
+  output:{
+    path:path.resolve(cwd,'dist'),
+    filename:'[name]-[hash:8].js'
+  },
+  module:require('../loaders/multi-loader.js'),
+  resolve:{
+    "alias":{
+      "widget":path.resolve(cwd,'./src/widget')
+    }
+  },
+  plugins: [
     new ExtractTextPlugin("[name]-[hash:8].css"),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
@@ -28,33 +43,18 @@ for(var i=0;i<configs.length;i++){
         NODE_ENV: JSON.stringify("development")
       }
     })
-  ].concat(config.plugins)
-
-  Object.assign(config,{
-    devtool: 'inline-source-map',
-    output:{
-      path:path.resolve(cwd,'dist'),
-      filename:'[name]-[hash:8].js'
-    },
-    module:require('../loaders/multi-loader.js'),
-    resolve:{
-      "alias":{
-        "widget":path.resolve(cwd,'./src/widget')
-      }
-    }
-  })
-  // 检查项目中是否定义了lib
-  if(lib){
-    for(let j in config.entry){
-      config.entry[j].zzzlib = lib
-    }
-    config.plugins.push(
-      new webpack.optimize.CommonsChunkPlugin({
-         names: ['zzzlib', 'manifest']
-      })
-    )
-  }
+  ].concat(htmlConfig)
 }
 
+// 检查项目中是否定义了lib
+var lib = require('./util.js').getLib()
+if(lib){
+  config.entry.zzzlib = lib
+  config.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+       names: ['zzzlib', 'manifest']
+    })
+  )
+}
 
-module.exports = configs
+module.exports = config
