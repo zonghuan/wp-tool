369dfcaeddb7eba7d3c06a92651d469104bc8b9d
housheng <housheng@qunhemail.com>
2018-05-22 17:59:49 +0800

    del es2015

--- loaders/spa-loader.js
+++ loaders/spa-loader.js
@@ -10,7 +10,7 @@ module.exports = {
           loader:require.resolve('babel-loader'),
           options:{
             presets: [
-              require.resolve('babel-preset-es2015'),
+          //    require.resolve('babel-preset-es2015'),
               require.resolve('babel-preset-react')
             ],
             babelrc: false
